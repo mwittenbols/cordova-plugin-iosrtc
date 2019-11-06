43f77029379d24662db64cbbf4e36e3a1a2d98a5
Manfred Wittenbols <info@motivatingconcepts.com>
2019-11-05 19:22:53 -0800

    original media stream has to be original types

--- dist/cordova-plugin-iosrtc.js
+++ dist/cordova-plugin-iosrtc.js
@@ -151,17 +151,13 @@ function newMediaStreamId() {
 var originalMediaStream = window.MediaStream;
 // Added 2019-10-08 by ROGDEV\Manfred:
 if (typeof originalMediaStream === "undefined") {
-	originalMediaStream = function () {
-		return { "dummy": "dummy"};
-	};
+	originalMediaStream = new MediaStream(null, 0);
 	
 }
 var originalMediaStreamTrack = MediaStreamTrack.originalMediaStreamTrack;
 // Added 2019-10-08 by ROGDEV\Manfred:
 if (typeof originalMediaStreamTrack === "undefined") {
-	originalMediaStreamTrack = function () {
-		return { "dummy": "dummy"};
-	};
+	originalMediaStreamTrack = new MediaStreamTrack(null);
 	
 }
 /**
